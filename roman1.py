e74861e91e0039aa86707b35e156607078d7fd17
Osama Alshaykh <osama@bu.edu>
2022-01-24 16:13:48 -0500

    Update roman1.py

--- roman1.py
+++ roman1.py
@@ -10,7 +10,8 @@ roman_numeral_map = (('M',  1000),
                      ('IX', 9),
                      ('V',  5),
                      ('IV', 4),
-                     ('I',  1))               
+                     ('I',  1))      
+import numpy
 
 def to_roman(n):
     '''convert integer to Roman numeral'''
